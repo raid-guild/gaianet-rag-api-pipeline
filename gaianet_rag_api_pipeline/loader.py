e4ba3498deacf89980d2d67f90e3979813aa519a
Santiago Gonzalez <hernangt12re3@gmail.com>
2024-09-08 00:30:47 -0500

    cleanup loader

--- gaianet_rag_api_pipeline/loader.py
+++ gaianet_rag_api_pipeline/loader.py
@@ -10,6 +10,96 @@ from typing import Any, Tuple
 import yaml
 
 
+def validate_base_definitions(mappings: dict):
+    defIds = list(mappings.get("definitions", {}).keys())
+    for refId in ["paginator", "requester_base", "retriever_base", "selector"]:
+        if refId not in defIds:
+            error_msg = f"{refId} is missing in mapping definitions"
+            logger.error(error_msg)
+            raise Exception(error_msg)
+
+
+def load_openapi_spec(openapi_spec_file: pathlib.Path) -> dict:
+    (openapi_spec, _) = read_from_filename(openapi_spec_file)
+    # validate openapi spec
+    try:
+        validate_spec(openapi_spec)
+    except Exception as error:
+        logger.error(f"Spec not valid. A {type(error).__name__} error was raised", exc_info=True)
+        raise error
+    
+    return openapi_spec
+
+
+def get_endpoints(mappings: dict):
+    endpoints = mappings.get("endpoints", {})
+    return {endpoint: resolve_refs(details, mappings) for endpoint, details in endpoints.items()}
+
+
+def get_pagination_schema(api_definitions: dict) -> PaginationSchemas:
+    pagination_strategy = api_definitions.\
+        get("paginator", {}).\
+        get("pagination_strategy", {}).\
+        get("type", None)
+
+    if pagination_strategy not in [schema.name for schema in PaginationSchemas]:
+        error_msg = f"Pagination strategy '{pagination_strategy}' not supported"
+        logger.error(error_msg)
+        raise Exception(error_msg)
+
+    return PaginationSchemas[pagination_strategy]
+
+
+def get_endpoint_response_schema(
+    endpoint_spec: dict,
+    request_method: str,
+    content_type: str,
+    response_entrypoint_field: str
+) -> dict:
+    response_schema = endpoint_spec.\
+        get(request_method, {}).\
+        get("responses", {}).get("200", {}).\
+        get("content", {}).get(content_type, {}).\
+        get("schema", {})
+
+    return response_schema.\
+        get("properties", {}).\
+        get(response_entrypoint_field, {})
+
+
+def get_response_data_fields(data_root: dict, entrypoint_type: str, openapi_spec: dict) -> dict:
+    data_fields = resolve_refs(data_root, openapi_spec)
+    if entrypoint_type == "array":
+        # need to get properties from nested items spec
+        data_fields = data_fields.\
+            get("items", {})
+    return data_fields.get("properties", {})
+
+
+def get_endpoint_text_properties(endpoint_details: dict, data_fields: dict) -> dict:
+    # should validate textSchemas exist in response schema
+    # fields specified in textSchemas will be extracted and joined together to be preprocessed
+    # other response data fields will be included as json metadata
+    text_schemas = endpoint_details.\
+        get("textSchema", {}).\
+        get("properties", {})
+    
+    fields_list = list(data_fields.keys())
+
+    # validate text properties are in the endpoint openapi spec as response data fields
+    text_properties = list()
+    for text_schema in text_schemas:
+        field = list(text_schema.keys())[0]
+        props = text_schema[field]
+        if field not in fields_list or props.get("type", "") != data_fields[field].get("type", ""):
+            error_msg = f"endpoint field not found or mismatch in openapi spec: {field} - {props}"
+            logger.error(error_msg)
+            raise Exception(error_msg)
+        text_properties.append(dict(field=field, **props))
+    
+    return text_properties
+
+
 def generate_source_manifest(
     mappings: dict,
     openapi_spec: dict,
@@ -31,8 +121,7 @@ def generate_source_manifest(
     source_parameters = list(spec_config_props.keys())
 
     # get endpoints and expand refs
-    endpoints = mappings.get("endpoints", {})
-    endpoints = {endpoint: resolve_refs(details, mappings) for endpoint, details in endpoints.items()}
+    endpoints = get_endpoints(mappings)
 
     # Config settings
     request_method = api_config.get("request_method", "get")
@@ -96,46 +185,25 @@ def generate_source_manifest(
         #####
 
         # get response schema from endpoint spec
-        response_schema = endpoint_spec.\
-            get(request_method, {}).\
-            get("responses", {}).get("200", {}).\
-            get("content", {}).get(content_type, {}).\
-            get("schema", {})
+        data_root = get_endpoint_response_schema(
+            endpoint_spec=endpoint_spec,
+            request_method=request_method,
+            content_type=content_type,
+            response_entrypoint_field=response_entrypoint_field
+        )
 
-        data_root = response_schema.\
-            get("properties", {}).\
-            get(response_entrypoint_field, {})
-
+        # get entrypoint type
         entrypoint_type = data_root.get("type", "")
-        #####
 
         # get response data fields
-        data_fields = resolve_refs(data_root, openapi_spec)
-        if entrypoint_type == "array":
-            # need to get properties from nested items spec
-            data_fields = data_fields.\
-                get("items", {})
-        data_fields = data_fields.get("properties", {})
-        fields_list = list(data_fields.keys())    
+        data_fields = get_response_data_fields(data_root, entrypoint_type, openapi_spec)
         logger.debug(f"endpoint spec data fields: {data_fields}")
 
-        # should validate textSchemas exist in response schema
-        # fields specified in textSchemas will be extracted and joined together to be preprocessed
-        # other response data fields will be included as json metadata
-        text_schemas = details.\
-            get("textSchema", {}).\
-            get("properties", {})
-
-        # validate text properties are in the endpoint openapi spec as response data fields
-        text_properties = list()
-        for text_schema in text_schemas:
-            field = list(text_schema.keys())[0]
-            props = text_schema[field]
-            if field not in fields_list or props.get("type", "") != data_fields[field].get("type", ""):
-                error_msg = f"endpoint field not found or mismatch in openapi spec: {field} - {props}"
-                logger.error(error_msg, exc_info=True)
-                raise Exception(error_msg)
-            text_properties.append(dict(field=field, **props))
+        # get endpoint text properties
+        text_properties = get_endpoint_text_properties(
+            endpoint_details=details,
+            data_fields=data_fields
+        )
         logger.debug(f"endpoint text fields: {text_properties}")
 
         # build endpoints ids
@@ -158,7 +226,7 @@ def generate_source_manifest(
             paginator = {
                 "$ref": "#/definitions/paginator"
             }
-        logger.debug(f"response schema: needs pagination? {needs_pagination} - {paginator} - {response_schema}")
+        logger.debug(f"response schema: needs pagination? {needs_pagination} - {paginator}")
         #####
 
         # setup requester
@@ -237,22 +305,10 @@ def api_loader(
     logger.debug(f"api parameters - {api_parameters}")
 
     # validate "base" definitions are in mappings
-    defIds = list(mappings.get("definitions", {}).keys())
-    for refId in ["paginator", "requester_base", "retriever_base", "selector"]:
-        if refId not in defIds:
-            error_msg = f"{refId} is missing in mapping definitions"
-            logger.error(error_msg, exc_info=True)
-            raise Exception(error_msg)
+    validate_base_definitions(mappings=mappings)
 
     # load openapi spec
-    (openapi_spec, _) = read_from_filename(openapi_spec_file)
-
-    # validate openapi spec
-    try:
-        validate_spec(openapi_spec)
-    except Exception as error:
-        logger.error(f"Spec not valid. A {type(error).__name__} error was raised", exc_info=True)
-        raise error
+    openapi_spec = load_openapi_spec(openapi_spec_file)
 
     # generate source manifest
     (source_manifest, endpoint_text_fields) = generate_source_manifest(
@@ -262,17 +318,7 @@ def api_loader(
 
     # get pagination strategy
     definitions = mappings.get("definitions", {})
-    pagination_strategy = definitions.\
-        get("paginator", {}).\
-        get("pagination_strategy", {}).\
-        get("type", None)
-
-    if pagination_strategy not in [schema.name for schema in PaginationSchemas]:
-        error_msg = f"Pagination strategy '{pagination_strategy}' not supported"
-        logger.error(error_msg, exc_info=True)
-        raise Exception(error_msg)
-
-    pagination_schema = PaginationSchemas[pagination_strategy]
+    pagination_schema = get_pagination_schema(api_definitions=definitions)
     logger.debug(f"pagination schema: {pagination_schema.name}")
 
     # store generated manifest
@@ -316,86 +362,49 @@ def api_read(
     with open(source_manifest_file, "r") as f:
         source_manifest = yaml.safe_load(f)
 
-    # TODO: set reusable validator
     # validate "base" definitions are in mappings
-    defIds = list(mappings.get("definitions", {}).keys())
-    for refId in ["paginator", "requester_base", "retriever_base", "selector"]:
-        if refId not in defIds:
-            error_msg = f"{refId} is missing in mapping definitions"
-            logger.error(error_msg, exc_info=True)
-            raise Exception(error_msg)
-    ######
+    validate_base_definitions(mappings=mappings)
 
-    # TODO: set reusable validator
     # load openapi spec
-    (openapi_spec, _) = read_from_filename(openapi_spec_file)
+    openapi_spec = load_openapi_spec(openapi_spec_file)
 
-    # validate openapi spec
-    try:
-        validate_spec(openapi_spec)
-    except Exception as error:
-        logger.error(f"Spec not valid. A {type(error).__name__} error was raised", exc_info=True)
-        raise error
-    ######
-
-    # TODO: set reusable getter
     # get endpoints and expand refs
-    endpoints = mappings.get("endpoints", {})
-    endpoints = {endpoint: resolve_refs(details, mappings) for endpoint, details in endpoints.items()}
-    ######
+    endpoints = get_endpoints(mappings)
 
-    # TODO: set reusable getter
+    # get pagination strategy
+    definitions = source_manifest.get("definitions", {})
+    pagination_schema = get_pagination_schema(api_definitions=definitions)
+    logger.debug(f"pagination schema: {pagination_schema.name}")
+
     # Config settings
     request_method = api_config.get("request_method", "get")
     content_type = api_config.get("content_type", "application/json")
     response_entrypoint_field = api_config.get("response_entrypoint_field", "data")
-    ######
 
     openapi_endpoints = openapi_spec.get("paths", {})
     endpoint_text_fields = dict()
-    # TODO: set reusable getters and validators
     for endpoint, details in endpoints.items():
         # get endpoint spec
         endpoint_spec = openapi_endpoints.get(endpoint, {})
         # get response schema from endpoint spec
-        response_schema = endpoint_spec.\
-            get(request_method, {}).\
-            get("responses", {}).get("200", {}).\
-            get("content", {}).get(content_type, {}).\
-            get("schema", {})
-        data_root = response_schema.\
-            get("properties", {}).\
-            get(response_entrypoint_field, {})
+        data_root = get_endpoint_response_schema(
+            endpoint_spec=endpoint_spec,
+            request_method=request_method,
+            content_type=content_type,
+            response_entrypoint_field=response_entrypoint_field
+        )
+        # get entrypoint type
         entrypoint_type = data_root.get("type", "")
-        #####
 
         # get response data fields
-        data_fields = resolve_refs(data_root, openapi_spec)
-        if entrypoint_type == "array":
-            # need to get properties from nested items spec
-            data_fields = data_fields.\
-                get("items", {})
-        data_fields = data_fields.get("properties", {})
-        fields_list = list(data_fields.keys())    
+        data_fields = get_response_data_fields(data_root, entrypoint_type, openapi_spec)
         logger.debug(f"endpoint spec data fields: {data_fields}")
 
-        # should validate textSchemas exist in response schema
-        # fields specified in textSchemas will be extracted to be preprocessed
-        # other response data fields will be included as json metadata
-        text_schemas = details.\
-            get("textSchema", {}).\
-            get("properties", {})
-
-        # validate text properties are in the endpoint openapi spec as response data fields
-        text_properties = list()
-        for text_schema in text_schemas:
-            field = list(text_schema.keys())[0]
-            props = text_schema[field]
-            if field not in fields_list or props.get("type", "") != data_fields[field].get("type", ""):
-                error_msg = f"endpoint field not found or mismatch in openapi spec: {field} - {props}"
-                logger.error(error_msg, exc_info=True)
-                raise Exception(error_msg)
-            text_properties.append(dict(field=field, **props))
+        # get endpoint text properties
+        text_properties = get_endpoint_text_properties(
+            endpoint_details=details,
+            data_fields=data_fields
+        )
         logger.debug(f"endpoint text fields: {text_properties}")
 
         # build endpoints ids
@@ -407,24 +416,6 @@ def api_read(
             entrypoint_type=entrypoint_type,
             text_properties=text_properties,
         )
-    ########
-
-    # TODO: set reusable getter and validator
-    # get pagination strategy
-    definitions = source_manifest.get("definitions", {})
-    pagination_strategy = definitions.\
-        get("paginator", {}).\
-        get("pagination_strategy", {}).\
-        get("type", None)
-
-    if pagination_strategy not in [schema.name for schema in PaginationSchemas]:
-        error_msg = f"Pagination strategy '{pagination_strategy}' not supported"
-        logger.error(error_msg, exc_info=True)
-        raise Exception(error_msg)
-
-    pagination_schema = PaginationSchemas[pagination_strategy]
-    logger.debug(f"pagination schema: {pagination_schema.name}")
-    ######
 
     # get chunking params
     chunking_params = mappings.get("chunking_params", {})
